fd7a571384d7a0a681ea55287fc7a1ad0f52ab05
sergezolo <zolotukhinsereg@gmail.com>
2020-07-07 21:38:25 -0500

    Modify '/goals/new' and '/goals/edit' to have many tasks

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,7 +2,7 @@ class GoalsController < ApplicationController
 
     get "/goals" do					
         if logged_in?				
-            @goals = current_user.goals		
+            @goals = current_user.goals
             erb :"goals/goals"			
         else				
             redirect to "/login"			
@@ -12,16 +12,16 @@ class GoalsController < ApplicationController
     get "/goals/new" do	
         if logged_in?				
             erb :"/goals/new"
-            binding.pry
         else				
             redirect to "/login"			
         end				
     end					
                         
     post "/goals" do		
-        if logged_in? && params[:title] != nil				
+        if logged_in? && params[:title] != nil
+            				
             @goal = current_user.goals.build(params)
-            @goal.tasks.build(params)
+            @task = @goal.tasks.build(params)
             if @goal.save 			
                 redirect to "/goals/#{@goal.id}"		
             else			
@@ -34,7 +34,7 @@ class GoalsController < ApplicationController
                    
     get "/goals/:id" do					
         if logged_in?				
-            @goal = current_user.goals.find_by_id(params[:id])			
+            @goal = current_user.goals.find_by_id(params[:id])
             if @goal			
                 erb :"/goals/show"		
             else			
@@ -47,7 +47,8 @@ class GoalsController < ApplicationController
                         
     get "/goals/:id/edit" do					
         if logged_in?				
-            @goal = current_user.goals.find_by_id(params[:id])			
+            @goal = current_user.goals.find_by_id(params[:id])
+            @goal.tasks
             if @goal		
                 erb :"/goals/edit"		
             else			
@@ -63,7 +64,16 @@ class GoalsController < ApplicationController
         if logged_in?				
             @goal = current_user.goals.find_by_id(params[:id])			
             if @goal && params[:title] != nil
-                @goal.update(title: params[:title], tasks: params[:tasks], notes: params[:notes])
+                @goal.title = params[:title]
+                @goal.tasks.task_1 = params[:task_1]
+                @goal.tasks.task_2 = params[:task_2]
+                @goal.tasks.task_3 = params[:task_3]
+                @goal.tasks.task_4 = params[:task_4]
+                @goal.tasks.task_5 = params[:task_5]
+                @goal.tasks.task_6 = params[:task_6]
+                @goal.tasks.task_7 = params[:task_7]
+                @goal.notes = params[:notes]
+                @goal.save
                 redirect to "/goals/#{@goal.id}"	
             else		
                 redirect to "/goals/#{@goal.id}/edit"	
